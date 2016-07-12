a0b2646ec1ab84ee3e91fcef29f778eb56ac2591
Andy S <aasfalcon@gmail.com>
2016-07-13 07:19:59 +0800

    starting path from command line

--- psb.py
+++ psb.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python
 
 import sys
+from os.path import expanduser
+
 from PyQt5.QtWidgets import QApplication
 from lib.window import Window
 
 if __name__ == "__main__":
+    path = sys.argv[1] if len(sys.argv) > 1 else expanduser('~')
     app = QApplication(sys.argv)
-    window = Window()
-    window.show()
+    window = Window(directory = path)
+    window.open()
     sys.exit(app.exec_())
