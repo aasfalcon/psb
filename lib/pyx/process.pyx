a4326a7e6d83e539b26e00e7a3c719b4b716a2a7
Andy S <aasfalcon@gmail.com>
2016-07-13 06:33:11 +0800

    Version 0.1.0

--- lib/pyx/process.pyx
+++ lib/pyx/process.pyx
@@ -2,38 +2,38 @@ import numpy as np
 from threading import Thread
 
 class PlayerBase(Thread):
-	level = .5
-	client = None
-	data = None
-	output = None
-	position = 0
-	channels = 2
-	
-				
-	def process(self, frames):
-		if self.data is None:
-			return
-			
-		(data_frames, data_channels) = self.data.shape
+    level = .5
+    client = None
+    data = None
+    output = None
+    position = 0
+    channels = 2
 
-		if self.position >= data_frames:
-			return
-			
-		pos = self.position
-		new_pos = pos + frames
-		length = frames if new_pos < data_frames else data_frames - pos			
-		output = self.output
 
-		for channel in range(self.channels):
-			data_channel = channel
-			
-			if data_channel >= data_channels:
-				if data_channels == 1:
-					data_channel = 0
-				else:
-					break
-					
-			output[:length] = self.data[pos:new_pos, data_channel] * self.level
-			self.client.outports[channel].get_buffer()[:] = output
-			
-		self.position = new_pos
+    def process(self, frames):
+        if self.data is None:
+            return
+
+        (data_frames, data_channels) = self.data.shape
+
+        if self.position >= data_frames:
+            return
+
+        pos = self.position
+        new_pos = pos + frames
+        length = frames if new_pos < data_frames else data_frames - pos
+        output = self.output
+
+        for channel in range(self.channels):
+            data_channel = channel
+
+            if data_channel >= data_channels:
+                if data_channels == 1:
+                    data_channel = 0
+                else:
+                    break
+
+            output[:length] = self.data[pos:new_pos, data_channel] * self.level
+            self.client.outports[channel].get_buffer()[:] = output
+
+        self.position = new_pos
--- setup.py
+++ setup.py
@@ -1,16 +1,37 @@
-from setuptools import setup, find_packages
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = 'Sample Browser',
-    version = '0.1',
-    long_description = __doc__,
-    packages = find_packages(),
+    version = '0.1.0',
+    author = "Andy S.",
+    author_email = "andrewjcarter@gmail.com",
+    packages = ['psb',  'lib'],
     include_package_data = True,
     zip_safe = False,
+    description = ("Simple sound file browser and previewer."),
+    long_description = read('README'),
+    license = "BSD",
+    keywords = "wav sample player file browser",
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+        "Environment :: X11 Applications :: Qt",
+        "Intended Audience :: End Users/Desktop",
+        "Topic :: Multimedia :: Sound/Audio :: Players",
+    ],
     install_requires = [
-		'JACK-client',
-		'numpy',
-		'soundfile',
-		'scikits.samplerate'
-	]
+        'JACK-client',
+        'numpy',
+        'soundfile',
+        'scikits.samplerate'
+    ]
 )
